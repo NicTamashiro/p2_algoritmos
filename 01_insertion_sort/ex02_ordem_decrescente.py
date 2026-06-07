5ed5326e86a76162602453139b8ade078640ee2a
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-07 09:27:11 -0300

    insertion sort decrescente em Py

--- 01_insertion_sort/ex02_ordem_decrescente.py
+++ 01_insertion_sort/ex02_ordem_decrescente.py
@@ -3,5 +3,18 @@ Exercicio 02
 Tema: 01_insertion_sort
 """
 
-if __name__ == "__main__":
-    pass
+def ordenar_crescente(arr, tamanho):
+    for i in range(1, tamanho):
+        temp = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] < temp:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = temp
+    
+arr = [3, 1, 7, 4]
+
+ordenar_crescente(arr, len(arr))
+
+print(arr)
