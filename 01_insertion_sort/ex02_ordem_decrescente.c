c5a1f6b1b074d59a15a29c4a8588093e4318791d
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-07 09:25:07 -0300

    insertion sort decrescente em C

--- 01_insertion_sort/ex02_ordem_decrescente.c
+++ 01_insertion_sort/ex02_ordem_decrescente.c
@@ -3,8 +3,31 @@ Exercicio 02
 Tema: 01_insertion_sort
 */
 
+void ordenar_decrescente(int * arr, int tamanho){
+    for(int i = 1; i < tamanho; i++){
+        int temp = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] < temp){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
 #include <stdio.h>
 
 int main() {
+
+    int arr[] = {3, 1, 7, 4};
+    int tamanho = sizeof(arr) / sizeof(arr[0]);
+
+    ordenar_decrescente(arr, tamanho);
+
+    for(int i = 0; i < tamanho; i++){
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
