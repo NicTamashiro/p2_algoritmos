6f24736ab50917f6ae5c769835800d3d876da0e5
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-07 09:18:02 -0300

    insertion sort crescente em C

--- 01_insertion_sort/ex01_insertion_sort_basico.c
+++ 01_insertion_sort/ex01_insertion_sort_basico.c
@@ -3,8 +3,31 @@ Exercicio 01
 Tema: 01_insertion_sort
 */
 
+void ordenar_crescente(int * arr, int tamanho){
+    for(int i = 0; i < tamanho; i++){
+        int temp = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > temp){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;    
+    }
+}
+
 #include <stdio.h>
 
 int main() {
+
+    int arr[] = {5, 2, 8, 1, 9};
+    int tamanho = sizeof(arr) / sizeof(arr[0]);
+
+    ordenar_crescente(arr, tamanho);
+
+    for(int i = 0; i < tamanho; i++){
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
