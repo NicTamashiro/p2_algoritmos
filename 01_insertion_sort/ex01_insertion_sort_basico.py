f4d3821b531d806d811cc1b657d8566121ed2c9f
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-07 09:20:12 -0300

    insertion sort crescente em Py

--- 01_insertion_sort/ex01_insertion_sort_basico.py
+++ 01_insertion_sort/ex01_insertion_sort_basico.py
@@ -3,5 +3,18 @@ Exercicio 01
 Tema: 01_insertion_sort
 """
 
-if __name__ == "__main__":
-    pass
+def ordenar_crescente(arr, tamanho):
+    for i in range(1, tamanho):
+        temp = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > temp:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = temp
+
+arr = [5, 2, 8, 1, 9]
+
+ordenar_crescente(arr, len(arr))
+
+print(arr)
