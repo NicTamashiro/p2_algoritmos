4d1cfc1412dfef8001cdf8909c8f97d2bf11d823
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 10:53:03 -0300

    criando uma função igual ao strlen em C

--- 04_strings/ex01_comprimento.c
+++ 04_strings/ex01_comprimento.c
@@ -5,6 +5,25 @@ Tema: 04_strings
 
 #include <stdio.h>
 
+int comprimento(char * string){
+    int contador = 0;
+    int i = 0;
+    while(string[i] != '\0') {
+        contador++;
+        i++;
+    }
+    return contador;
+}
+
 int main() {
+
+    char string[100];
+
+    scanf("%s", string);
+
+    int tamanho = comprimento(string);
+
+    printf("Tamanho da string: %d\n", tamanho);
+
     return 0;
 }
