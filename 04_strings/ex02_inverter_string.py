5ce39dd489b72fa1ef78e4c73a6d298915d5e523
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 17:00:36 -0300

    Inverter string em Py

--- 04_strings/ex02_inverter_string.py
+++ 04_strings/ex02_inverter_string.py
@@ -3,5 +3,18 @@ Exercicio 02
 Tema: 04_strings
 """
 
-if __name__ == "__main__":
-    pass
+def inverter(nome):
+    letras = list(nome)
+    inicio = 0
+    fim = len(nome) - 1
+
+    while inicio < fim:
+        letras[inicio], letras[fim] = letras[fim], letras[inicio]
+        inicio += 1
+        fim -= 1
+    
+    return "".join(letras)
+
+nome = "Nicolas"
+
+inverter(nome)
