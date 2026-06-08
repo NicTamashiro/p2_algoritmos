e6dfcadd11d6c1a27e1977551c7cbd9e57c77411
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 16:06:00 -0300

    calculando soma e media em C

--- 05_vetores/ex02_soma_media.c
+++ 05_vetores/ex02_soma_media.c
@@ -5,6 +5,29 @@ Tema: 05_vetores
 
 #include <stdio.h>
 
+float soma(float * arr, int tamanho){
+    float soma = 0;
+
+    for(int i = 0; i < tamanho; i++){
+        soma += arr[i];
+    }
+    return soma;
+}
+
+float media(float * arr, int tamanho){
+    return soma(arr, tamanho) / tamanho;
+}
+
 int main() {
+
+    float arr[] = {2.0, 4.0, 6.0};
+    int tamanho = sizeof(arr) / sizeof(arr[0]);
+
+    float sum = soma(arr, tamanho);
+    float average = media(arr, tamanho);
+
+    printf("Soma: %.2f\n", sum);
+    printf("Media: %.2f\n", average);
+
     return 0;
 }
