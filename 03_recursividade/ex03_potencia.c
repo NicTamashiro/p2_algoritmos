c1df8cf1789c91bfe4bc0d0e4e5e1a16ff5d0fd3
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 18:46:32 -0300

    Função recursiva potencia em C

--- 03_recursividade/ex03_potencia.c
+++ 03_recursividade/ex03_potencia.c
@@ -5,6 +5,35 @@ Tema: 03_recursividade
 
 #include <stdio.h>
 
+int potencia(int base, int exp){
+    if (exp == 0) return 1;
+    else if (exp == 1) return base;
+
+    return base * potencia(base, exp - 1);
+}
+
 int main() {
+
+    int base;
+    int exp;
+
+    printf("Digite a base e o expoente: \n");
+    scanf("%d %d", &base, &exp);
+
+    int resultado = potencia(base, exp);
+
+    printf("%d^%d = %d\n", base, exp, resultado);
+
     return 0;
 }
+
+/*
+
+    2^4
+
+    2 * potencia(2, 3)
+             2    *    potencia(2, 2)
+                            2     *     potencia(2, 1)
+                                                2
+
+*/
