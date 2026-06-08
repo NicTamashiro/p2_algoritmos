8b2e75d96518d66948bf7a8dae17a2096dc25b28
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 16:45:45 -0300

    remover duplicados em C

--- 05_vetores/ex05_remover_duplicatas.c
+++ 05_vetores/ex05_remover_duplicatas.c
@@ -5,6 +5,31 @@ Tema: 05_vetores
 
 #include <stdio.h>
 
+int remove_dup(int * arr, int tamanho){
+    if(tamanho == 0) return 0;
+
+    int escrita = 0;
+
+    for(int i = 1; i < tamanho; i++){
+        if(arr[i] != arr[escrita])
+            escrita++;
+            arr[escrita] = arr[i];
+    }
+
+    return escrita + 1;
+}
+
 int main() {
+
+    int arr[] = {1,1,2,3,3,4};
+    int tamanho = sizeof(arr) / sizeof(arr[0]);
+
+    int new_size = remove_dup(arr, tamanho);
+
+    printf("New size = %d\n", new_size);
+    for(int i = 0; i < tamanho; i++){
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
