ebfd1f66bda289919e633971b0b5f9be74bd61a7
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 15:53:50 -0300

    exercicio que retorna maior e menor num de um array

--- 05_vetores/ex01_maior_menor.c
+++ 05_vetores/ex01_maior_menor.c
@@ -3,8 +3,42 @@ Exercicio 01
 Tema: 05_vetores
 */
 
+int maior(int * num, int tamanho){
+    int maior = num[0];
+
+    for(int i = 1; i < tamanho; i++){
+        if(num[i] > maior){
+            maior = num[i];
+        }
+    }
+
+    return maior;
+}
+
+int menor(int * num, int tamanho){
+    int menor = num[0];
+
+    for(int i = 1; i < tamanho; i++){
+        if(num[i] < menor){
+            menor = num[i];
+        }
+    }
+
+    return menor;
+}
+
 #include <stdio.h>
 
 int main() {
+
+    int num[] = {3,7,1,9,4};
+    int tamanho = sizeof(num) / sizeof(num[0]);
+
+    int highest = maior(num, tamanho);
+    int worst = menor(num, tamanho);
+
+    printf("Maior = %d\n", highest);
+    printf("Menor = %d\n", worst);
+
     return 0;
 }
