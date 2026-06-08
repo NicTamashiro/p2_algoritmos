7e4eda5fbf447b67e785f10577522d253f264738
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 17:00:29 -0300

    inverter string em C

--- 04_strings/ex02_inverter_string.c
+++ 04_strings/ex02_inverter_string.c
@@ -4,7 +4,29 @@ Tema: 04_strings
 */
 
 #include <stdio.h>
+#include <string.h>
+
+void inverter_str(char * string){
+    int inicio = 0;
+    int fim = strlen(string) - 1;
+
+    for(int i = 0; i < strlen(string)/2; i++){
+        char temp = string[inicio];
+        string[inicio] = string[fim];
+        string[fim] = temp;
+
+        inicio++;
+        fim--;
+    }
+}
 
 int main() {
+
+    char string[20] = "Nicolas";
+
+    inverter_str(string);
+
+    printf("%s", string);
+
     return 0;
 }
