4254572cfd85679e2056eda522528d6714b71f2c
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 16:49:38 -0300

    remover duplicados em Py

--- 05_vetores/ex05_remover_duplicatas.py
+++ 05_vetores/ex05_remover_duplicatas.py
@@ -3,5 +3,18 @@ Exercicio 05
 Tema: 05_vetores
 """
 
-if __name__ == "__main__":
-    pass
+def remover_duplicadas(v):
+    resultado = []
+
+    for elemento in v:
+        if elemento not in resultado:
+            resultado.append(elemento)
+    
+    return resultado
+
+v = [1,1,2,3,3,4]
+v = remover_duplicadas(v)
+
+print(f"Tamanho: {len(v)}")
+print(f"Vetor: {v}")
+
