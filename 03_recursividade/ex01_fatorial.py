b45c8860f11466d8e8010eb015abc259e91a890e
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 10:41:01 -0300

    Fatorial recursiva em Py

--- 03_recursividade/ex01_fatorial.py
+++ 03_recursividade/ex01_fatorial.py
@@ -3,5 +3,11 @@ Exercicio 01
 Tema: 03_recursividade
 """
 
-if __name__ == "__main__":
-    pass
+def fatorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fatorial(n - 1)
+
+n = 5
+print(fatorial(n))
