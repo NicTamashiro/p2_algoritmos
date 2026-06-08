3f6f65164a536b69e6ac1048b960849a634a9ab0
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 16:18:39 -0300

    busca linear em Py

--- 05_vetores/ex03_busca_linear.py
+++ 05_vetores/ex03_busca_linear.py
@@ -3,5 +3,18 @@ Exercicio 03
 Tema: 05_vetores
 """
 
-if __name__ == "__main__":
-    pass
+def busca_linear(arr, tamanho, target):
+    for i in range(0, tamanho):
+        if arr[i] == target:
+            return i
+    return -1
+
+arr = [5,3,8,1,7]
+target = 8
+
+resultado = busca_linear(arr, len(arr), target)
+
+if resultado == -1:
+    print("Elemento nao encontrado")
+else:
+    print(f"Elemento '{target}' encontrado na posicao {resultado}")
