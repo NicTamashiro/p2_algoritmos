41b4caf833e4ccd3c1fd6eb5fb9516eff553d006
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 17:40:06 -0300

    Se string eh palindromo em Py

--- 04_strings/ex03_palindromo.py
+++ 04_strings/ex03_palindromo.py
@@ -3,5 +3,22 @@ Exercicio 03
 Tema: 04_strings
 """
 
-if __name__ == "__main__":
-    pass
+def palindromo(string):
+    inicio = 0
+    fim = len(string) - 1
+
+    while inicio <= fim:
+        if string[inicio].lower() != string[fim].lower():
+            return 0
+        inicio += 1
+        fim -= 1
+    return 1
+
+string = "Arara"
+
+resultado = palindromo(string)
+
+if resultado == 1:
+    print("Eh palindromo")
+else:
+    print("Nao eh palindromo")
