4777531a7577a0e085a6883d1283ecb9aa61b70c
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 10:39:47 -0300

    Fatorial com recursividade em C

--- 03_recursividade/ex01_fatorial.c
+++ 03_recursividade/ex01_fatorial.c
@@ -5,6 +5,18 @@ Tema: 03_recursividade
 
 #include <stdio.h>
 
+int fatorial(int num){
+    if(num == 1) return 1;
+    else return num * fatorial(num - 1);
+}
+
 int main() {
+
+    int num = 5;
+
+    int resultado = fatorial(num);
+
+    printf("Fatorial = %d\n", resultado);
+
     return 0;
 }
