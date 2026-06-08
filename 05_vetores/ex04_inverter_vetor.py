3efdd8b29162f2f63cb8675884bb197a216bf627
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 16:30:25 -0300

    invertendo vetor em Py

--- 05_vetores/ex04_inverter_vetor.py
+++ 05_vetores/ex04_inverter_vetor.py
@@ -3,5 +3,20 @@ Exercicio 04
 Tema: 05_vetores
 """
 
-if __name__ == "__main__":
-    pass
+def inverting_arr(arr, size):
+    start = 0
+    end = size - 1
+
+    for i in range(0, size//2):
+        temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+
+        start += 1
+        end -= 1
+
+arr = [1,2,3,4,5]
+
+inverting_arr(arr, len(arr))
+
+print(arr)
