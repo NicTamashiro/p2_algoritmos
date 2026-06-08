b994cab3870183ad84c2a6479ce19d47b8e15639
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 15:58:29 -0300

    exercicio que retorna maior e menor num de um array em Py

--- 05_vetores/ex01_maior_menor.py
+++ 05_vetores/ex01_maior_menor.py
@@ -3,5 +3,29 @@ Exercicio 01
 Tema: 05_vetores
 """
 
-if __name__ == "__main__":
-    pass
+def maior(num, tamanho):
+    maior = num[0]
+
+    for i in range(1,tamanho):
+        if num[i] > maior:
+            maior = num[i]
+        
+    return maior
+
+def menor(num, tamanho):
+    menor = num[0]
+
+    for i in range(1, tamanho):
+        if num[i] < menor:
+            menor = num[i]
+    
+    return menor
+
+
+num = [3,7,1,9,4]
+
+highest = maior(num, len(num))
+worst = menor(num, len(num))
+
+print(f"Maior = {highest}")
+print(f"Menor = {worst}")
