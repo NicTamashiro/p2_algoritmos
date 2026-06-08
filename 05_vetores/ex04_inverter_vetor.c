cf1d5a6a4828aa310ec101951c9fa249fdc3a13d
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 16:26:47 -0300

    invertendo vetor em C

--- 05_vetores/ex04_inverter_vetor.c
+++ 05_vetores/ex04_inverter_vetor.c
@@ -5,6 +5,30 @@ Tema: 05_vetores
 
 #include <stdio.h>
 
+void inverting_arr(int * arr, int size){
+    int start = 0;
+    int end = size - 1;
+
+    for(int i = 0; i < size/2; i++){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }   
+}
+
 int main() {
+
+    int arr[] = {1,2,3,4,5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    inverting_arr(arr, size);
+
+    for(int i = 0; i < size; i++){
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
