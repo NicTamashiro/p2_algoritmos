d079df2563321934b0b268e4cff5b1008dfa154e
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 18:49:00 -0300

    Função recursiva potencia em Py

--- 03_recursividade/ex03_potencia.py
+++ 03_recursividade/ex03_potencia.py
@@ -3,5 +3,15 @@ Exercicio 03
 Tema: 03_recursividade
 """
 
-if __name__ == "__main__":
-    pass
+def potencia(base, exp):
+    if exp == 0:
+        return 1
+    elif exp == 1:
+        return base
+    
+    return base * potencia(base, exp - 1)
+
+base = int(input("Digite a base: "))
+exp = int(input("Digite o expoente: "))
+
+print(f"{base}^{exp} = {potencia(base, exp)}")
