fbf351b1f3710c886921e986ea1fb94da561333a
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 16:09:09 -0300

    calculando soma e media em Py

--- 05_vetores/ex02_soma_media.py
+++ 05_vetores/ex02_soma_media.py
@@ -3,5 +3,18 @@ Exercicio 02
 Tema: 05_vetores
 """
 
-if __name__ == "__main__":
-    pass
+def soma(arr, tamanho):
+    soma = 0
+
+    for i in range(0, tamanho):
+        soma += arr[i]
+    
+    return soma
+
+def media(arr, tamanho):
+    return soma(arr, tamanho) / tamanho
+
+arr = [2.0, 4.0, 6.0]
+
+print(f"Soma = {soma(arr, len(arr)):.2f}")
+print(f"Media = {media(arr, len(arr)):.2f}")
