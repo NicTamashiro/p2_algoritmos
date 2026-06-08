1d617efc4e10d485f5ecda56b1afcaca4a838d93
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-08 18:36:46 -0300

    Fibonacci recursivo em C

--- 03_recursividade/ex02_fibonacci.c
+++ 03_recursividade/ex02_fibonacci.c
@@ -5,6 +5,21 @@ Tema: 03_recursividade
 
 #include <stdio.h>
 
+int fib(int n){
+    if (n == 1){
+        return 0;
+    } else if (n == 2){
+        return 1;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
+
+    int n = 5;
+
+    printf("O fibonacci de '%d' eh = %d\n", n, fib(n));
+
     return 0;
 }
