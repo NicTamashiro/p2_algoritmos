8bd469770163ffc3b74dc020c603e4882b9cddf2
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-09 10:00:20 -0300

    calculadora em Py

--- 07_funcoes/ex01_calculadora.py
+++ 07_funcoes/ex01_calculadora.py
@@ -3,5 +3,34 @@ Exercicio 01
 Tema: 07_funcoes
 """
 
-if __name__ == "__main__":
-    pass
+def somar(n1, n2, op):
+    print(f"{n1} + {n2} = {n1+n2}")
+def subtrair(n1, n2, op):
+    print(f"{n1} - {n2} = {n1-n2}")
+def multiplicar(n1, n2, op):
+    print(f"{n1} * {n2} = {n1*n2}")
+def dividir(n1, n2, op):
+    if n2 == 0:
+        print("Nao pode dividir numero por '0'")
+        return
+    print(f"{n1} / {n2} = {n1/n2}")
+
+n1 = int(input("Digite o primeiro numero: "))
+op = input("Digite a operacao: ")
+n2 = int(input("Digite o segundo numero: "))
+
+match op:
+    case '+':
+        somar(n1,n2,op)
+        
+    case '-':
+        subtrair(n1,n2,op)
+        
+    case '*':
+        multiplicar(n1,n2,op)
+        
+    case '/':
+        dividir(n1,n2,op)
+        
+    case _:
+        print("Operacao invalida!")
