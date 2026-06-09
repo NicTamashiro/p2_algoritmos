0cacb4dc134e12faacf735ea1b8e08de18a72c52
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-09 17:18:10 -0300

    Adapte o algoritmo de ordenação por inserção gerando um vetor em ordem decrescente. em Py

---
+++ Slides/slide8-3.py
@@ -0,0 +1,15 @@
+def ordenar_decrescente(arr, tamanho):
+    for i in range(1, tamanho):
+        temp = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] < temp:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = temp
+
+arr = [3,1,7,5,4,9]
+
+ordenar_decrescente(arr, len(arr))
+
+print(arr)
