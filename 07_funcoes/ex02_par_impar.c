bc928e1b2d748d7efc30cabf0a0a6f3cae99d3da
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-09 10:08:04 -0300

    funcao ehpar e ehimpar

--- 07_funcoes/ex02_par_impar.c
+++ 07_funcoes/ex02_par_impar.c
@@ -5,6 +5,34 @@ Tema: 07_funcoes
 
 #include <stdio.h>
 
+int ehimpar(int n){
+    if(n % 2 != 0){
+        return 1;
+    } else{
+        return 0;
+    }
+}
+
+int ehpar(int n){
+    if (n % 2 == 0){
+        return 1;
+    } else if(ehimpar(n) == 1){
+        return 0;
+    }
+}
+
+
 int main() {
+
+    int n;
+    printf("Digite um numero para saber se eh 'par' ou 'impar':\n");
+    scanf("%d", &n);
+
+    if (ehpar(n) == 1){
+        printf("'%d' eh par\n", n);
+    } else {
+        printf("'%d' eh impar\n", n);
+    }
+
     return 0;
 }
