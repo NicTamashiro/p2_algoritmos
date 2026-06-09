c9ac2e91dff5a4df1cd948271aa400bdca743637
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-09 10:10:37 -0300

    funcao ehpar e ehimpar em Py

--- 07_funcoes/ex02_par_impar.py
+++ 07_funcoes/ex02_par_impar.py
@@ -3,5 +3,22 @@ Exercicio 02
 Tema: 07_funcoes
 """
 
-if __name__ == "__main__":
-    pass
+def ehimpar(n):
+    if n % 2 != 0:
+        return 1
+    else:
+        return 0
+    
+def ehpar(n):
+    if n % 2 == 0:
+        return 1
+    else:
+        return 0
+
+n = int(input("Digite um numero para saber se eh 'par' ou 'impar': "))
+
+if ehpar(n) == 1:
+    print(f"'{n}' eh par")
+else:
+    print(f"'{n}' eh impar")
+
