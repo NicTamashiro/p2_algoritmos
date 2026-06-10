9e8b9e729f6a62bf683c1f6a584d5fa3e170458c
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-10 10:03:55 -0300

    Exercicio de concatenar em Py

--- funcoes_string/concatenacao.py
+++ funcoes_string/concatenacao.py
@@ -1,3 +1,8 @@
 # Exercício 4 — Python
 # Leia nome e sobrenome e use + para montar
 # o nome completo em uma nova string.
+
+nome = input("Nome: ")
+sobrenome = input("Sobrenome: ")
+completo = nome + " " + sobrenome
+print("Nome completo:", completo)
