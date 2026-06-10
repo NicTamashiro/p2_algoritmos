4ccacd51e83cc0dcee989e60639aba13d2124aad
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-10 10:16:03 -0300

    Funcao strlen

--- funcoes_string/strlen.c
+++ funcoes_string/strlen.c
@@ -1,3 +1,18 @@
 /* Exercício 1 — C
    Leia uma palavra digitada pelo usuário e exiba
    quantas letras ela possui usando strlen(). */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+   char nome[50];
+   scanf("%49[^\n]", nome);
+
+   int tamanho = strlen(nome);
+
+   printf("Tamanho = %d\n", tamanho);
+
+   return 0;
+}
