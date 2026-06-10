3d8aab66561bc80b34d1e77c30e4b683d035a57a
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-10 10:28:03 -0300

    Leia uma frase e use strtok() para contar quantas palavras ela contém, em C

--- funcoes_string/strtok2.c
+++ funcoes_string/strtok2.c
@@ -1,3 +1,26 @@
 /* Exercício 6 — C
    Leia uma frase e use strtok() para contar
    quantas palavras ela contém. */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+   char frase[51];
+   scanf("%50[^\n]", frase);
+
+   int contador = 0;
+   char * tok;
+
+   tok = strtok(frase, " ");
+
+   for(int i = 0; tok != NULL; i++){
+      contador++;
+      tok = strtok(NULL, " ");
+   }
+
+   printf("Quantidade = %d\n", contador);
+
+   return 0;
+}
