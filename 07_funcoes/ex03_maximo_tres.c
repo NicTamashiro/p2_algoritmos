43ae9da5281a7c4327a8320069b6c744dc4eef72
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-10 11:41:18 -0300

    exercicio de funcao que calcula o maior em C

--- 07_funcoes/ex03_maximo_tres.c
+++ 07_funcoes/ex03_maximo_tres.c
@@ -5,6 +5,27 @@ Tema: 07_funcoes
 
 #include <stdio.h>
 
+int maior(int * arr){
+    int maior = arr[0];
+
+    for(int i = 1; i < 3; i++){
+        if (arr[i] > maior){
+            maior = arr[i];
+        }
+    }
+    return maior;
+}
+
 int main() {
+
+    int arr[3];
+    for(int i = 0; i < 3; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    int resultado = maior(arr);
+
+    printf("%d", resultado);
+
     return 0;
 }
