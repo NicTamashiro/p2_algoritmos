72df7cb659ef066f3288fb542f8b341028f6c3a2
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-10 10:06:52 -0300

    Comparacao de Strings em C

--- funcoes_string/comparacao.c
+++ funcoes_string/comparacao.c
@@ -1,3 +1,22 @@
 /* Exercício 2 — C
    Leia duas strings e informe se são iguais ou
    diferentes usando strcmp(). */
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+   char str1[30], str2[30];
+
+   scanf("%s %s", str1, str2);
+
+   if(strcmp(str1, str2) == 0){
+      printf("Sao iguais!\n");
+   } else {
+      printf("Sao diferentes!\n");
+   }
+
+   return 0;
+}
