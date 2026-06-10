666d97f2adbeefbad10cd640985ccd13961c7a86
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-10 10:12:15 -0300

    Funcao strcpy em C

--- funcoes_string/copia.c
+++ funcoes_string/copia.c
@@ -1,3 +1,19 @@
 /* Exercício 3 — C
    Leia um nome, copie para outra variável com strcpy()
    e exiba as duas confirmando que são independentes. */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+   char nome[50], copia[50];
+
+   scanf("%s", nome);
+   strcpy(copia, nome);
+
+   printf("Original: %s\n", nome);
+   printf("Copia: %s\n", copia);
+
+   return 0;
+}
