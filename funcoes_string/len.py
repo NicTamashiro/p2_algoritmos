fca377d542639f6377ecac0f8f1826ebd53c599d
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-10 10:17:23 -0300

    Funcao len python

--- funcoes_string/len.py
+++ funcoes_string/len.py
@@ -1,3 +1,8 @@
 # Exercício 1 — Python
 # Leia uma palavra digitada pelo usuário e exiba
 # quantas letras ela possui usando len().
+
+palavra = input("Digite uma palavra para saber a qtd de caracteres: ")
+tamanho = len(palavra)
+
+print("Tamanho:", tamanho)
