52cf06fc0f7fcfb13e67c547ed19ac6778753574
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-10 10:08:15 -0300

    comparacao de Strings em Py

--- funcoes_string/comparacao.py
+++ funcoes_string/comparacao.py
@@ -1,3 +1,11 @@
 # Exercício 2 — Python
 # Leia duas strings e informe se são iguais ou
 # diferentes usando o operador ==.
+
+str1 = input("Primeira string: ")
+str2 = input("Segunda string: ")
+
+if str1 == str2:
+    print("Sao iguais!")
+else:
+    print("Sao diferentes!")
