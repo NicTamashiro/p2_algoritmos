d513d751bd5b3d8832a0a6ae8c8b3073d171f7ef
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-10 11:38:58 -0300

    exercicio de funcao que calcula o maior em Py

--- 07_funcoes/ex03_maximo_tres.py
+++ 07_funcoes/ex03_maximo_tres.py
@@ -3,5 +3,17 @@ Exercicio 03
 Tema: 07_funcoes
 """
 
-if __name__ == "__main__":
-    pass
+def maior(arr):
+    maior = arr[0]
+
+    for i in range(1, 3):
+        if arr[i] > maior:
+            maior = arr[i]
+    return maior
+
+arr = [0]*3
+
+for i in range(3):
+    arr[i] = int(input())
+
+print(maior(arr))
