43f1525fcdd60e34536c0b44de2601a7ca71e80c
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-10 10:24:20 -0300

    Leia uma frase e use strtok() para separar e imprimir cada palavra em uma linha diferente, usando o espaço como delimitador. em C

--- funcoes_string/strtok1.c
+++ funcoes_string/strtok1.c
@@ -2,3 +2,30 @@
    Leia uma frase e use strtok() para separar e
    imprimir cada palavra em uma linha diferente,
    usando o espaço como delimitador. */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+   char frase[101];
+   char partes[10][30];
+   char * tok;
+   int contador = 0;
+
+   scanf("%100[^\n]", frase);
+
+   tok = strtok(frase, " ");
+
+   for(int i = 0; tok != NULL; i++){
+      strcpy(partes[i], tok);
+      contador++;
+      tok = strtok(NULL, " ");
+   }
+
+   for(int i = 0; i < contador; i++){
+      printf("Palavra '%d': %s\n", i,partes[i]);
+   }
+
+   return 0;
+}
